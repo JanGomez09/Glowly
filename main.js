76c41d86780810e71d90a27381e7591ecbf098af
JanGomez <jan.estebanrg@gmail.com>
2026-05-04 08:57:34 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+const toogleButton = document.querySelector('.toggle');
+
+
+toogleButton.addEventListener("click", () => {
+    const main = document.querySelector('.main');
+    main.classList.toggle('main2');
+ 
+}); 
